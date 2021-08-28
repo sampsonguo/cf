561ee9804b9f69c5fb11e3099d5789fd339655f1
sampsonguo <1141482126@qq.com>
2021-08-28 10:31:46 +0800

    update

--- 740/a.cc
+++ 740/a.cc
@@ -1,18 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool check(vector<int>& a) {
+    for(int i = 0; i < a.size()-1; i++) if(a[i] > a[i+1]) return false;
+    return true;
+}
+
+void print(vector<int>& a) {
+    for(int i = 0; i < a.size(); i++) cout << a[i] << " ";
+    cout << endl;
+}
+
 void solve() {
     int n;
     cin >> n;
     vector<int> a(n);
     for(int i = 0; i < n; i++) cin >> a[i];
     bool flag = true;
-    int iter = 1;
     int ret = 0;
     while(flag) {
         flag = false;
-        for(int i = 0; i < n-1; i+=2) if(a[i] > a[i+1]) swap(a[i], a[i+1]), ret++, iter++, flag = true;
-        for(int i = 1; i < n-1; i+=2) if(a[i] > a[i+1]) swap(a[i], a[i+1]), ret++, iter++, flag = true;
+        bool add1 = false;
+        bool add2 = false;
+
+        if(check(a)) break;
+        for(int i = 0; i < n-1; i+=2) if(a[i] > a[i+1]) swap(a[i], a[i+1]), add1 = true, flag = true;
+        ret++;
+        //print(a);
+
+        if(check(a)) break;
+        for(int i = 1; i < n-1; i+=2) if(a[i] > a[i+1]) swap(a[i], a[i+1]), add2 = true, flag = true;
+        ret++;
+        //print(a);
     }
     cout << ret << endl;
     return ;
